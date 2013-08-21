eba14435061149fafb6b69fef3c9cfcad46bcda7
Cesar Redondo <revuls@gmail.com>
2013-08-21 15:50:19 -0400

    deleted files

--- Routers/index.router.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// GET index route
-$app->get('/', function () use ($app) {
-    $hello = "hello world!";
-    //echo $hello;
-    $app->render('index.html', array('hello' => $hello));
-});
