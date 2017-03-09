5003ce09bd12a8ea8292ab40c4974df148e4a7f9
arvin <arvin@almar.io>
2017-03-09 13:51:34 +0800

    Environment Based configuration and autoload config based on environment

--- lib/Config.php
+++ lib/Config.php
@@ -6,11 +6,16 @@ namespace lib;
 class Config {
     static $confArray;
 
-    public static function read($name) {
-        return self::$confArray[$name];
+    public static function read($name) 
+    {
+        if(isset(self::$confArray[APPLICATION_ENV][$name])){
+            return self::$confArray[APPLICATION_ENV][$name];    
+        }
+        return self::$confArray['local'][$name];
     }
 
-    public static function write($name, $value) {
-        self::$confArray[$name] = $value;
+    public static function write($name, $value) 
+    {
+            self::$confArray[APPLICATION_ENV][$name] = $value;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
+// Get Env variable to automatically include environment config
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? 
+                                  getenv('APPLICATION_ENV') : 
+                                  'local'));
+
+// show errors when working on local
+if(APPLICATION_ENV === 'local'){
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
 require '../vendor/autoload.php';
-require '../config.php';
+require '../configs/'.strtolower(APPLICATION_ENV).'.config.php';
 
 // Setup custom Twig view
 $twigView = new \Slim\Views\Twig();
